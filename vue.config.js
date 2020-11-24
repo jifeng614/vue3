c7517bf70071285d3b284befeb8a3276b8a8b53d
jifeng614 <2298724990@qq.com>
2020-11-24 15:19:06 +0800

    vue3

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'https://www.wumeili.top',
+                changeOrigin: true,
+                secure: false,
+                pathRewrite: {
+                    '^/api': 'https://www.wumeili.top'
+                }
+            }
+        }
+    }
